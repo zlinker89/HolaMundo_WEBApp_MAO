2df37ddaaf5b29a51c586f49a970a5fb2d3f2cd1
Franklin ospino <ospi89@hotmail.com>
2017-03-09 22:24:53 -0500

    release v1.0.0

--- Gruntfile.js
+++ Gruntfile.js
@@ -392,7 +392,12 @@ module.exports = function (grunt) {
           cwd: 'bower_components/bootstrap/dist',
           src: 'fonts/*',
           dest: '<%= yeoman.dist %>'
-        }]
+        }, { 
+  expand: true,
+  cwd: '<%= yeoman.app %>/bower_components/font-awesome', 
+  src: 'fonts/*', 
+  dest: '<%= yeoman.dist %>' 
+}]
       },
       styles: {
         expand: true,
